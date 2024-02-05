b66bf19a6b4f6c9d7e7adfaa8822f760f89019f4
Zee Sun <zsunusa95@gmail.com>
2024-02-04 21:00:41 -0800

    Update YelpRepositoryImplTest.kt

--- app/src/test/java/com/example/yelpapipractice/feature/yelp/data/repository/YelpRepositoryImplTest.kt
+++ app/src/test/java/com/example/yelpapipractice/feature/yelp/data/repository/YelpRepositoryImplTest.kt
@@ -20,7 +20,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
-@RunWith(JUnit4::class)
 class YelpRepositoryImplTest {
 
     private val testDispatcher = StandardTestDispatcher()
